3b43818bbc91b5e156a2c4b0566edbb3deb1283a
Vshnv2001 <mvaishnav.sg@gmail.com>
2022-07-24 09:51:05 +0800

    Added GET request to retrieve user's NUSMods Link

--- timeslots/views.py
+++ timeslots/views.py
@@ -6,24 +6,33 @@ from .helperfunctions.timetableparser import *
 import pandas as pd
 from rest_framework.decorators import api_view
 
-@api_view(['POST', 'PUT'])
+@api_view(['POST', 'PUT', 'GET'])
 def user_settings(request : HttpRequest):
-    username = request.POST.get('username')
-    modslink = request.POST.get('modslink')
-    gym_name = request.POST.get('gym_name')
-    days = request.POST.get('days')
-    day_time = request.POST.get('day_time')
-    if UserSettings.objects.filter(username = username).exists():
-        user_row : UserSettings
-        user_row = UserSettings.objects.get(username = username)
-        user_row.mods_link = modslink
-        user_row.days = days
-        user_row.gym_name = gym_name
-        user_row.day_time = day_time
-    else:
-        user_row = UserSettings(username = username, mods_link = modslink, days = days, day_time = day_time)
-    user_row.save()
-    return HttpResponse(status=200)
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        modslink = request.POST.get('modslink')
+        gym_name = request.POST.get('gym_name')
+        days = request.POST.get('days')
+        day_time = request.POST.get('day_time')
+        if UserSettings.objects.filter(username = username).exists():
+            user_row : UserSettings
+            user_row = UserSettings.objects.get(username = username)
+            user_row.mods_link = modslink
+            user_row.days = days
+            user_row.gym_name = gym_name
+            user_row.day_time = day_time
+        else:
+            user_row = UserSettings(username = username, mods_link = modslink, days = days, day_time = day_time)
+        user_row.save()
+        return HttpResponse(status=200)
+    elif request.method == 'GET':
+        username = request.GET.get('username')
+        if UserSettings.objects.filter(username = username).exists():
+            modslink = UserSettings.objects.get(username = username).mods_link
+            return HttpResponse(modslink)
+        else:
+            return HttpResponse('')
+
  
 @api_view(['GET'])
 def to_df(request: HttpRequest):
